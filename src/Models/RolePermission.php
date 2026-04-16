0b0c30441faf5b54fd2fb84e79be58b4cec3dcb8
andis <1271292479@qq.com>
2026-04-16 16:34:02 +0800

    ok

--- src/Models/RolePermission.php
+++ src/Models/RolePermission.php
@@ -7,7 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class RolePermission extends Model
 {
     protected $table = 'system_role_permission';
-    protected $guarded = [];
+
+    protected $guarded = ['id']; //不可以注入的数据字段
+
+    protected function serializeDate(\DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
 
 
 }
